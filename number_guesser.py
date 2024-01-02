6cf24979e17be6d3ace1e81bfa4e07bbdc0ecd85
epmt6528 <epmt6528@gmail.com>
2024-01-02 14:46:14 -0800

    Reviewed and updated number guesser

--- number_guesser.py
+++ number_guesser.py
@@ -1,28 +1,21 @@
 import random
 
-top_of_range = input("Type a number: ")
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
+def get_valid_input(prompt):
+    while True:
+        value = input(prompt)
+        if value.isdigit() and int(value) > 0:
+            return int(value)
+        print("Please enter a positive number.")
 
-    if top_of_range <= 0:
-        print("Please type a number larger than 0 next time.")
-        quit()
-else:
-    print("Please type a number next time.")
-    quit()
 
+top_of_range = get_valid_input("Type a number: ")
 random_number = random.randint(0, top_of_range)
 guesses = 0
 
 while True:
     guesses += 1
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    else:
-        print("Please type a number next time.")
-        continue
+    user_guess = get_valid_input("Make a guess: ")
 
     if user_guess == random_number:
         print("You got it!")

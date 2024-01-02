447d37113d8666b5011e08f6aaa4d799bb4eb941
epmt6528 <epmt6528@gmail.com>
2024-01-02 14:18:38 -0800

    Reviewed and updated quiz game

--- quiz_game.py
+++ quiz_game.py
@@ -1,42 +1,27 @@
 print("Welcome to my computer quiz!")
-
 playing = input("Do you want to play? ")
 
-if playing.lower() != "yes":
+if playing.strip().lower() not in ["yes", "y"]:
+    print("Okay! Maybe next time :)")
     quit()
 
 print("Okay! Let's play :)")
 
 score = 0
+quiz = [
+    {"quiz": "What does CPU stand for? ", "answer": "central processing unit"},
+    {"quiz": "What does GPU stand for? ", "answer": "graphic processing unit"},
+    {"quiz": "What does RAM stand for? ", "answer": "random access memory"},
+    {"quiz": "What does PSU stand for? ", "answer": "power supply"},
+]
 
-answer = input("What does CPU stand for? ")
-if answer.lower() == "central processing unit":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect!")
+for question in quiz:
+    answer = input(question["quiz"])
+    if answer.strip().lower() == question["answer"]:
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect!")
 
-answer = input("What does GPU stand for? ")
-if answer.lower() == "graphic processing unit":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("What does RAM stand for? ")
-if answer.lower() == "random access memory":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("What does PSU stand for? ")
-if answer.lower() == "power supply":
-    print("Correct!")
-    score += 1
-else:
-    print("Incorrect!")
-
-
-print("You got " + str(score) + " questions correct!")
-print("You got " + str((score/4)*100) + "%.")
+print(f"You got {score} question(s) correct!")
+print(f"You got {(score/4)*100}%.")
